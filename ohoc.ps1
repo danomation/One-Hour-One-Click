5264f732d8adf5506a757dfd05539b3a469e65be
Dan <dluttrell87@gmail.com>
2024-01-28 14:48:51 -0500

    Update ohoc.ps1

--- ohoc.ps1
+++ ohoc.ps1
@@ -27,10 +27,8 @@ Read-Host -Prompt "<enter to continue>"
 $container = docker ps -a -q -f name=ohoc
 if($container.Length -GE 1){
     echo "Container already exists.... restarting"
-    docker container restart ohoc
-    docker exec -d ohoc /bin/bash -c "cd /home/ohol/OneLife/server/ && nohup ./OneLifeServer \&; exec bash"
-    echo "Server is running."
-}
+    docker container start ohoc
+    docker exec -it ohoc /bin/bash -c "cd /home/ohol/OneLife/server/ && echo 'type ./OneLifeServer to start server'; exec bash"}
 else{
     docker run -p 8005:8005 -itd --name=ohoc --tty ubuntu:22.04 /bin/bash
     docker exec -it ohoc /bin/bash -c "apt -y update; apt -y upgrade; apt-get -y update; apt-get -y upgrade; apt-get -y install build-essential; cd /home; mkdir ohol; cd ohol; apt-get -y install wget; wget https://raw.githubusercontent.com/danomation/onehouroneclick/main/pullAndBuildTestSystem.sh && bash pullAndBuildTestSystem.sh; exec bash"
